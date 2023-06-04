9bc851d84e05c2ec0918d5565c06951ed7a275b8
Ygor Araujo <ygorcavalcantib@gmail.com>
2023-06-04 13:46:56 -0300

    Iniciando o projeto editor de texto criado menu e o metodo sair

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: Sair(); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Abrir()
+        {
+
+        }
+
+        static void Editar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o seu texto abaixo");
+            Console.WriteLine("-------------------------");
+        }
+        static void Sair()
+        {
+            Console.Clear();
+            Console.WriteLine("Você escolheu sair...");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("Salvando arquivos necessarios...");
+            Thread.Sleep(800);
+            Console.Clear();
+            Console.WriteLine("Fechando arquivos necessarios...");
+            Thread.Sleep(700);
+            Console.Clear();
+            Console.WriteLine("Obrigado por usar nosso sistema, estamos anciosos pela sua volta!");
+            Thread.Sleep(1000);
+            Console.Clear();
+            System.Environment.Exit(0);
+        }
+    }
+}
